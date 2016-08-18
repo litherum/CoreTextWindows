28b385161ae5aba306f1dee2b2b7e5a01783709c
Myles C. Maxfield <mmaxfield@apple.com>
2016-08-17 22:04:20 -0700

    Initial Commit

--- CoreTextWindows/CoreTextWindows.cpp
+++ CoreTextWindows/CoreTextWindows.cpp
@@ -3,6 +3,33 @@
 
 #include "stdafx.h"
 #include "CoreTextWindows.h"
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <CoreGraphics/CoreGraphics.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreText/CoreText.h>
+
+extern "C" {
+
+	typedef const struct __CTFont* CTFontRef;
+	typedef const struct __CTLine* CTLineRef;
+	typedef const struct __CTRun* CTRunRef;
+	typedef const struct __CTFontDescriptor* CTFontDescriptorRef;
+
+	const CFStringRef kCTFontAttributeName = CFSTR("NSFont");
+	const CFStringRef kCTForegroundColorAttributeName = CFSTR("CTForegroundColor");
+
+	CTFontRef CTFontCreateWithName(CFStringRef name, CGFloat size, const CGAffineTransform* matrix);
+	CTLineRef CTLineCreateWithAttributedString(CFAttributedStringRef);
+	void CTLineDraw(CTLineRef, CGContextRef);
+	CFArrayRef CTLineGetGlyphRuns(CTLineRef line);
+	CFIndex CTRunGetGlyphCount(CTRunRef run);
+	void CTRunGetGlyphs(CTRunRef run, CFRange range, CGGlyph buffer[]);
+	void CTRunGetPositions(CTRunRef run, CFRange range, CGPoint buffer[]);
+	CGFontRef CTFontCopyGraphicsFont(CTFontRef font, CTFontDescriptorRef* attributes);
+	void CTRunDraw(CTRunRef run, CGContextRef context, CFRange range);
+}
 
 #define MAX_LOADSTRING 100
 
@@ -144,9 +171,105 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_PAINT:
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+
+			RECT windowRect;
+			::GetClientRect(hWnd, &windowRect);
+			LONG width = windowRect.right - windowRect.left;
+			LONG height = windowRect.bottom - windowRect.top;
+
+			BITMAPINFO bitmapInfo = { 0 };
+			bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+			bitmapInfo.bmiHeader.biWidth = width;
+			bitmapInfo.bmiHeader.biHeight = height;
+			bitmapInfo.bmiHeader.biPlanes = 1;
+			bitmapInfo.bmiHeader.biBitCount = 32;
+			bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+			void* bits;
+			HBITMAP bitmap = CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, &bits, 0, 0);
+
+			HDC compatibleDC = ::CreateCompatibleDC(hdc);
+			HGDIOBJ oldBitmap = ::SelectObject(compatibleDC, bitmap);
+
+			assert(GetCurrentObject(compatibleDC, OBJ_BITMAP) == bitmap);
+			BITMAP usedBitmap = { 0 };
+			GetObject(bitmap, sizeof(usedBitmap), &usedBitmap);
+			assert(usedBitmap.bmBits == bits);
+			assert(usedBitmap.bmBitsPixel == 32);
+
+			CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+			CGContextRef context = CGBitmapContextCreate(usedBitmap.bmBits, usedBitmap.bmWidth, usedBitmap.bmHeight, 8, usedBitmap.bmWidthBytes, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipFirst);
+			CGContextTranslateCTM(context, 0, usedBitmap.bmHeight);
+			CGContextScaleCTM(context, 1, -1);
+			SetGraphicsMode(compatibleDC, GM_ADVANCED);
+
+			CGFloat redComponents[] = { 1, 0, 0, 1 };
+			CGColorRef red = CGColorCreate(colorSpace, redComponents);
+			CGContextSetFillColorWithColor(context, red);
+			CGContextSetStrokeColorWithColor(context, red);
+
+			CGContextFillRect(context, CGRectMake(100, 200, 300, 400));
+
+			CGFloat greyComponents[] = { 0.5, 0.5, 0.5, 1 };
+			CGColorRef grey = CGColorCreate(colorSpace, greyComponents);
+			CGContextSetFillColorWithColor(context, grey);
+			CGContextSetStrokeColorWithColor(context, grey);
+
+			CTFontRef font = CTFontCreateWithName(CFSTR("Arial"), 100, nullptr);
+			CFTypeRef keys[] = { kCTFontAttributeName, kCTForegroundColorAttributeName };
+			CFTypeRef values[] = { font, grey };
+			CFDictionaryRef attributes = CFDictionaryCreate(kCFAllocatorDefault, keys, values, sizeof(keys) / sizeof(keys[0]), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+			CFAttributedStringRef attributedString = CFAttributedStringCreate(kCFAllocatorDefault, CFSTR("Hello, World!"), attributes);
+			CTLineRef line = CTLineCreateWithAttributedString(attributedString);
+			CGFontRef cgFont = CTFontCopyGraphicsFont(font, nullptr);
+
+			CGContextSetFont(context, cgFont);
+			CGContextSetFontSize(context, 100);
+			CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+			CGContextSetTextPosition(context, 100, 100);
+
+			CTLineDraw(line, context);
+
+			CFArrayRef runs = CTLineGetGlyphRuns(line);
+			CFIndex runCount = CFArrayGetCount(runs);
+			for (CFIndex i = 0; i < runCount; ++i) {
+				CTRunRef run = static_cast<CTRunRef>(CFArrayGetValueAtIndex(runs, i));
+				CFIndex glyphCount = CTRunGetGlyphCount(run);
+				std::cout << "Run " << i << " has " << glyphCount << " glyphs." << std::endl;
+				std::vector<CGGlyph> glyphs(glyphCount);
+				CTRunGetGlyphs(run, CFRangeMake(0, 0), glyphs.data());
+				std::vector<CGPoint> positions(glyphCount);
+				CTRunGetPositions(run, CFRangeMake(0, 0), positions.data());
+				for (CFIndex j = 0; j < glyphCount; ++j) {
+					std::cout << "Glyph index " << j << ": " << glyphs[j] << " at position (" << positions[j].x << ", " << positions[j].y << ")" << std::endl;
+				}
+				//CGContextShowGlyphsAtPositions(context, glyphs.data(), positions.data(), glyphCount);
+				//CTRunDraw(run, context, CFRangeMake(0, 0));
+			}
+
+			CGFontRelease(cgFont);
+			CFRelease(line);
+			CFRelease(attributedString);
+			CFRelease(attributes);
+			CFRelease(font);
+
+			CGColorRelease(red);
+			CGColorRelease(grey);
+
+			CGContextRelease(context);
+			CGColorSpaceRelease(colorSpace);
+
+			GdiFlush();
+
+			assert(windowRect.left == 0 && windowRect.top == 0);
+			BitBlt(hdc, windowRect.left, windowRect.top, width, height, compatibleDC, windowRect.left, windowRect.top, SRCCOPY);
+
+			::SelectObject(compatibleDC, oldBitmap);
+			::DeleteDC(compatibleDC);
+			::DeleteObject(bitmap);
+
             EndPaint(hWnd, &ps);
         }
         break;
